a578f711fc7c870fd3e40907baaaaa9685e57840
Sarah Autumn <neverether@gmail.com>
2019-08-09 14:32:35 -0700

    tweaking and testing

--- bsplease.sh
+++ bsplease.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES=21*.pdf
+FILES=./the-gauntlet/21*.pdf
 TEMPDIR=./temp
 
 mkdir -p ./temp
@@ -27,33 +27,35 @@ do
 
 		# Extract just the portions of the page we are interested in.
 		printf "Extracting interesting pieces of the page...\n"
-		mogrify -gravity North\
-						-density 400x400\
-						-units PixelsPerInch\
-						-crop '100%x25%'\
-						-write $TEMPDIR/3_titlestrip.png\
+		mogrify -gravity North \
+						-density 400x400 \
+						-units PixelsPerInch \
+						-crop '100%x25%' \
+						-write $TEMPDIR/3_titlestrip.png \
 						$TEMPDIR/2_convert.png
 
-		mogrify -gravity East\
-						-density 800x800\
-						-units PixelsPerInch\
-						-crop '25%x100%+0-500'\
-						-write $TEMPDIR/4_numberdateiss.png\
+		mogrify -gravity East \
+						-density 800x800 \
+						-units PixelsPerInch \
+						-crop '25%x100%+0-500' \
+						-write $TEMPDIR/4_numberdateiss.png \
 						$TEMPDIR/3_titlestrip.png
 
-		mogrify -gravity Center\
-						-density 400x400\
-						-units PixelsPerInch\
-						-crop '2190x690+100+150'\
-						-write $TEMPDIR/5_title.png\
+		mogrify -gravity Center \
+						-density 400x400 \
+						-units PixelsPerInch \
+						-crop '2230x690+30+150' \
+						-write $TEMPDIR/5_title.png \
 						$TEMPDIR/3_titlestrip.png
 
-		printf "Masking off lower left corner to fix text recognition...\n\n"
+		printf "Masking off areas where title is unlikely to appear...\n\n"
 		mogrify -gravity Center \
 						-size 2190x690 \
-						-fill 'rgba( 255, 215, 0 , 1 )' \
-						-draw 'rectangle 0,550 150,690' \
-						-draw 'rectangle 1600,550 2190,690' \
+						-fill 'rgba( 255, 215, 215 , 1 )' \
+						-draw 'rectangle 0,0 120,60' \
+						-draw 'rectangle 0,550 160,690' \
+						-draw 'rectangle 1800,550 2230,690' \
+						-draw 'rectangle 0,660 2190,690' \
 						-write $TEMPDIR/6_title_masked.png \
 						$TEMPDIR/5_title.png
 
