d8cbafc60046b6660e2cde09791baabacbf90891
Sarah Autumn <neverether@gmail.com>
2019-08-09 00:19:35 -0700

    made output quieter, better recognition

--- bsplease.sh
+++ bsplease.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES=218-122*.pdf
+FILES=21*.pdf
 TEMPDIR=./temp
 
 mkdir -p ./temp
@@ -7,13 +7,13 @@ mkdir -p ./temp
 for f in $FILES
 do
 
-		echo "Working on "$f
+		printf "\n\nWorking on "$f
 		filename=$(basename $f)
 		sectionno=${filename:0:11}
 
 		# gs extracts the first page of the PDF
-		printf  "Ghostscripting...\n\n"
-		gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER \
+		printf  "\nGhostscripting...\n"
+		gs -q -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER \
 		   -dFirstPage=1 \
 		   -dLastPage=1 \
 		   -sOutputFile=$TEMPDIR/1_gs.pdf \
@@ -21,56 +21,58 @@ do
 
 
 		# Convert that single page to jpg
-		printf "\nConverting PDF --> PNG...\n"
+		printf "Converting PDF --> PNG...\n"
 		convert -density 400 $TEMPDIR/1_gs.pdf $TEMPDIR/2_convert.png
-		printf "done\n\n"
+		printf "done\n"
 
 		# Extract just the portions of the page we are interested in.
-		printf "Extracting interesting pieces of the page...\n\n"
+		printf "Extracting interesting pieces of the page...\n"
 		mogrify -gravity North\
 						-density 400x400\
 						-units PixelsPerInch\
 						-crop '100%x25%'\
-						-write $TEMPDIR/titlestrip.png\
+						-write $TEMPDIR/3_titlestrip.png\
 						$TEMPDIR/2_convert.png
 
 		mogrify -gravity East\
 						-density 800x800\
 						-units PixelsPerInch\
 						-crop '25%x100%+0-500'\
-						-write $TEMPDIR/numberdateiss.png\
-						$TEMPDIR/titlestrip.png
+						-write $TEMPDIR/4_numberdateiss.png\
+						$TEMPDIR/3_titlestrip.png
 
 		mogrify -gravity Center\
 						-density 400x400\
 						-units PixelsPerInch\
-						-crop '2190x690+50+150'\
-						-write $TEMPDIR/title.png\
-						$TEMPDIR/titlestrip.png
+						-crop '2190x690+100+150'\
+						-write $TEMPDIR/5_title.png\
+						$TEMPDIR/3_titlestrip.png
 
 		printf "Masking off lower left corner to fix text recognition...\n\n"
 		mogrify -gravity Center \
 						-size 2190x690 \
 						-fill 'rgba( 255, 215, 0 , 1 )' \
-						-draw 'rectangle 0,550 100,690' \
-						-write $TEMPDIR/title.png \
-						$TEMPDIR/title.png
+						-draw 'rectangle 0,550 150,690' \
+						-write $TEMPDIR/6_title_masked.png \
+						$TEMPDIR/5_title.png
 
-		tesseract $TEMPDIR/title.png $TEMPDIR/title bazaar
-		tesseract $TEMPDIR/numberdateiss.png $TEMPDIR/numberdateiss bsp_number
+		tesseract $TEMPDIR/6_title_masked.png $TEMPDIR/title bazaar quiet
+		tesseract $TEMPDIR/4_numberdateiss.png $TEMPDIR/numberdateiss bsp_number\
+						quiet
 
-		printf "\n\n Title and Section\n"
+		printf "Title and Section\n"
 		printf '%s\n' '-----------------------'
 
 		echo $sectionno
 		echo $sectionno >> output.txt
 		printf "\n"
 		echo "" >> output.txt
-		cat $TEMPDIR/title.txt | sed -e 's/NO\. I/NO. 1/g' -e '/^$/q' |\
-				sed -e '/^$/d'
+		cat $TEMPDIR/title.txt | sed -e 's/NO\. I/NO. 1/g' -e '/^$/q' \
+				-e 's/§/5/g' |  sed -e '/^$/d'
 
-		cat $TEMPDIR/title.txt | sed -e 's/NO\. I/NO. 1/g' -e '/^$/q' |\
-				sed -e '/^$/d' >> output.txt
+
+		cat $TEMPDIR/title.txt | sed -e 's/NO\. I/NO. 1/g' -e '/^$/q' \
+				-e 's/§/5/g' |  sed -e '/^$/d' >> output.txt
 
 		echo "" >> output.txt
 #		cat numberdateiss.txt
